78d4ef62bae727eb18ad466d307c97d6a78548b5
Ruma Das <rumaadl16@gmail.com>
2020-07-16 13:47:24 +0930

    adding connection

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
- app.get('/', (req, res) =>{
-     res.send("HelloWorld");
-     console.log("server is Connected");
- })
- const PORT =5000;
-app.listen(PORT)
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
+
+app.get('/', (req, res) => {
+    res.send("HelloWorld yess....");
+
+})
+
+app.listen(PORT, () => console.log(`server is started at: ${PORT}`));
